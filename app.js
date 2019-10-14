3259684a3df5b6f054c0fe205593f4ac9cc35aad
itebk <khayati.iteb@gmail.com>
2019-10-14 16:48:45 +0100

    2nd commit

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 var express = require('express');
 // Set up the express app
 const app = express();
-
+const pdf2html = require('pdf2html')
 //requiring path and fs modules
 const path = require('path');
 const fs = require('fs');
+
 //joining path of directory 
 const directoryPath = path.join(__dirname, '../DocSimilarity/src/assets/NLP_data/input_pdfs');
+
+//joining path of files 
+const filePathJson = path.join(__dirname, '../DocSimilarity/src/assets/NLP_data/cleaned_text.json');
+const refsPathJson = path.join(__dirname, '../DocSimilarity/src/assets/NLP_data/cleaned_refs.json');
+
+
+//get file by name
+app.get('/api/file/:name', (req, res) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    var name = req.params.name;
+    var path = require('path');     
+    var file = path.join(directoryPath, name);    
+    res.download(file, function (err) {
+           if (err) {
+               console.log("Error");
+               console.log(err);
+           } else {
+               console.log("Success");
+           }    
+    });
+
+});
+
 
 // get all files
 app.get('/api/files', (req, res) => {
@@ -23,6 +48,54 @@ app.get('/api/files', (req, res) => {
             success: 'true',
             message: 'files retrieved successfully',
             files: files
+        })
+    });
+
+
+});
+
+
+
+// get json text 
+app.get('/api/text', (req, res) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+ 
+    //passsing filePath and callback function
+    fs.readFile(filePathJson, function (err, text) {
+        //handling error
+        if (err) {
+            return console.log('Unable to read file: ' + err);
+        }
+        res.status(200).send({
+            success: 'true',
+            message: 'text retrieved successfully',
+            text: JSON.parse(text)
+        })
+    });
+
+
+});
+
+
+
+// get json refs 
+app.get('/api/refs', (req, res) => {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+ 
+    //passsing filePath and callback function
+    fs.readFile(refsPathJson, function (err, refs) {
+        //handling error
+        if (err) {
+            return console.log('Unable to read file: ' + err);
+        }
+        res.status(200).send({
+            success: 'true',
+            message: 'refs retrieved successfully',
+            refs: JSON.parse(refs)
         })
     });
 
